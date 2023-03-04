e11cf10696b2301b13272b336ba98ce8c560ebf9
Changhoon Han <shirohoo4102@gmail.com>
2023-03-04 23:02:31 +0900

    :recycle: groups in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
 }
 
-group = "org.demo"
+group = "io.github.olivahn"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
